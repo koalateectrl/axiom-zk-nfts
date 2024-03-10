f9313b723c0f26c3d0ce44424a8577d7607cc28b
kphed <kpcrypto69@gmail.com>
2024-03-10 16:32:15 -0700

    Init ZKColors contract

---
+++ src/ZKColors.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC721} from "solady/tokens/ERC721.sol";
+
+contract ZKColors is ERC721 {
+    /// @dev Returns the token collection name.
+    function name() public pure override returns (string memory) {
+        return "ZKColors";
+    }
+
+    /// @dev Returns the token collection symbol.
+    function symbol() public pure override returns (string memory) {
+        return "ZKC";
+    }
+
+    /// @dev Returns the Uniform Resource Identifier (URI) for token `id`.
+    function tokenURI(
+        uint256 id
+    ) public view override returns (string memory) {}
+}
