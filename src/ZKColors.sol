b6c70a577e9401f3c99c5aa38526dc178643ed34
kphed <kpcrypto69@gmail.com>
2024-03-10 17:25:03 -0700

    Update metadata serving methods

--- src/ZKColors.sol
+++ src/ZKColors.sol
@@ -24,39 +24,34 @@ contract ZKColors is ERC721 {
         return "ZKC";
     }
 
+    /// @dev Returns base64-encoded SVG with the token ID's color.
     function _getImage(uint256 id) private view returns (string memory) {
         return
             string(
                 abi.encodePacked(
-                    '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 3000 3000">',
-                    '<rect width="100%" height="100%" fill="',
-                    _colors[id] == Colors.RED ? "red" : "green",
-                    '" />',
-                    "</svg>"
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(
+                        abi.encodePacked(
+                            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 3000 3000">',
+                            '<rect width="100%" height="100%" fill="',
+                            _colors[id] == Colors.RED ? "red" : "green",
+                            '" />',
+                            "</svg>"
+                        )
+                    )
                 )
             );
     }
 
     /// @dev Returns the Uniform Resource Identifier (URI) for token `id`.
     function tokenURI(uint256 id) public view override returns (string memory) {
-        bytes memory dataURI = abi.encodePacked(
-            "{",
-            '"name": "ZKColors',
-            '",',
-            '"description": "Axiom ZK intensive cohort #2 by KoalateeCtrl, ppmoon69, SaganLives, and Naruto11.",',
-            '"image_data": "',
+        string memory json = string.concat(
+            '{"name": "ZKColors","description":"Testtesttest","image":"',
             _getImage(id),
-            '"',
-            "}"
+            '"}'
         );
 
-        return
-            string(
-                abi.encodePacked(
-                    "data:application/json;base64,",
-                    Base64.encode(dataURI)
-                )
-            );
+        return string.concat("data:application/json;utf8,", json);
     }
 
     function mint(Colors color) external {
