d8a48e32830598a252aedbd6e46edf5c42074342
koalateectrl <koalateectrl@gmail.com>
2024-03-20 21:23:27 -0700

    update circuit

--- app/axiom/balance.circuit.ts
+++ app/axiom/balance.circuit.ts
@@ -27,9 +27,9 @@ export interface CircuitInputs {
 // Default inputs to use for compiling the circuit. These values should be different than the inputs fed into
 // the circuit at proving time.
 export const defaultInputs = {
-  "blockNumber": 5493970,
-  "token1Addr": "0x898A7c662f7D1aD119A168C688E11148189b1b72",
-  "token2Addr": "0xfbD52Ea21Ddff75EE829BEbA42A483Bf23622DD7",
+  "blockNumber": 5494070,
+  "token1Addr": "0xd04D6724Fb95BAda8ef799Ff3E2Cb4728c9f145D",
+  "token2Addr": "0x8B17E3629C1253805dbDBAFee6C7382aB5F1Bf75",
   "userAddr": "0xcD39fEbf4709c7727a72B8C177D141771a9c9554",
   "tokenId": 1
 }
@@ -38,24 +38,39 @@ export const defaultInputs = {
 // change the function name, you'll also need to ensure that you also pass the Axiom CLI flag
 // `-f <circuitFunctionName>` for it to work
 export const circuit = async (inputs: CircuitInputs) => {
-  const mappingBeforeToken1 = getSolidityMapping(sub(inputs.blockNumber, 1000), inputs.token1Addr, 0);
-  const balanceBefore1 = await mappingBeforeToken1.nested([inputs.userAddr]);
-  const mappingAfterToken1 = getSolidityMapping(inputs.blockNumber, inputs.token1Addr, 0);
-  const balanceAfter1 = await mappingAfterToken1.nested([inputs.userAddr]);
-  const averageBalanceToken1 = div(add(balanceBefore1.toCircuitValue(), balanceAfter1.toCircuitValue()), 2);
-  const mappingBeforeToken2 = getSolidityMapping(sub(inputs.blockNumber, 1000), inputs.token2Addr, 0);
-  const balanceBefore2 = await mappingBeforeToken2.nested([inputs.userAddr]);
-  const mappingAfterToken2 = getSolidityMapping(inputs.blockNumber, inputs.token2Addr, 0);
-  const balanceAfter2 = await mappingAfterToken2.nested([inputs.userAddr]);
-  const afterVaaverageBalanceToken2 = div(add(balanceBefore2.toCircuitValue(), balanceAfter2.toCircuitValue()), 2);
+  
+  // Obtaining the average for token 1
+  const currToken1Mapping = getSolidityMapping(inputs.blockNumber, inputs.token1Addr, 0);
+  const currToken1Balance256 = await currToken1Mapping.nested([inputs.userAddr]);
+  let sumToken1Balance = currToken1Balance256.toCircuitValue();
+
+  for (let i = 1; i < 10; i++) {
+      let token1Mapping = getSolidityMapping(sub(inputs.blockNumber, mul(i, 100)), inputs.token1Addr, 0);
+      let token1Balance = await token1Mapping.nested([inputs.userAddr]);
+      sumToken1Balance = add(sumToken1Balance, token1Balance.toCircuitValue());
+  }
+
+  const average1Balance = div(sumToken1Balance, 10);
+
+  // Obtaining the average for token 2
+  const currToken2Mapping = getSolidityMapping(inputs.blockNumber, inputs.token2Addr, 0);
+  const currToken2Balance256 = await currToken2Mapping.nested([inputs.userAddr]);
+  let sumToken2Balance = currToken2Balance256.toCircuitValue();
+
+  for (let i = 1; i < 10; i++) {
+      let token2Mapping = getSolidityMapping(sub(inputs.blockNumber, mul(i, 100)), inputs.token2Addr, 0);
+      let token2Balance = await token2Mapping.nested([inputs.userAddr]);
+      sumToken2Balance = add(sumToken2Balance, token2Balance.toCircuitValue());
+  }
+
+  const average2Balance = div(sumToken2Balance, 10);
 
   addToCallback(inputs.blockNumber);
   addToCallback(inputs.token1Addr);
   addToCallback(inputs.token2Addr);
   addToCallback(inputs.userAddr);
   addToCallback(inputs.tokenId);
-
-  addToCallback(averageBalanceToken1);
-  addToCallback(afterVaaverageBalanceToken2);
+  addToCallback(average1Balance);
+  addToCallback(average2Balance);
 
 };
